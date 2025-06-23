ccea87f594b138d400c9c063fd946e11a6b8167b
shaibwoiUn0h <maxthompson786@gmail.com>
2025-06-23 18:10:56 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+// MobilityX - React Native App (MVP)
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Welcome to MobilityX!</Text>
+    </View>
+  );
+}
